52e1981dae653a470a15b4a1406d8a89456b4571
ziennaa <manyakalra817@gmai.com>
2025-06-28 20:22:44 +0530

    moved images path and updated index.html and Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header(){
     return(
         <header>
-        <img src="./cheficon.jpeg" alt="logo"/>
+        <img src="/cheficon.jpeg" alt="logo"/>
         <h1>Chef Mixtral</h1>
         </header>
     )
